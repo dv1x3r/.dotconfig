f74a196c760c3a0a20d9d875fca8aab7a08d9651
dv1x3r <d.purvinsh@gmail.com>
2022-11-16 15:31:52 +0200

    add file path nvim

--- dotfiles/nvim.lua
+++ dotfiles/nvim.lua
@@ -84,7 +84,16 @@ require("packer").startup(function(use)
 		requires = { "ryanoasis/vim-devicons" },
 		config = function()
 			require("lualine").setup({
-				-- options = { globalstatus = true }
+				-- options = { globalstatus = true },
+				sections = {
+					lualine_c = {
+						{
+							"filename",
+							file_status = true, -- displays file status (readonly status, modified status)
+							path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+						},
+					},
+				},
 			})
 		end,
 	})
