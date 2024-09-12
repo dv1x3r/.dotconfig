03d64838b9a511a7934c010a137d4c7d8a1655e8
dv1x3r <d.purvinsh@gmail.com>
2024-09-12 11:35:20 +0300

    dotconfig update

--- Makefile.go
+++ Makefile.go
@@ -1,13 +1,10 @@
-include .env
-
-APP_NAME ?= main
-GOOSE=goose -dir=./migrations ${GOOSE_DRIVER} ${GOOSE_DBSTRING}
+GOOSE=goose -dir=./migrations sqlite ./tmp/data.db
 
 build:
-	go build -o bin/$(APP_NAME)
+	go build -o tmp/server
 
 run:
-	go build -o ./bin/$(APP_NAME) && ./bin/$(APP_NAME)
+	go build -o ./tmp/server && ./tmp/server
 
 test:
 	go test -v ./...
@@ -15,12 +12,12 @@ test:
 db-up:
 	$(GOOSE) up
 
-db-up-by-one:
-	$(GOOSE) up-by-one
-
 db-up-to:
 	@read -p "Up to version: " VALUE; \
 	$(GOOSE) up-to $$VALUE
+
+db-up-by-one:
+	$(GOOSE) up-by-one
 
 db-down:
 	$(GOOSE) down
@@ -29,11 +26,11 @@ db-down-to:
 	@read -p "Down to version: " VALUE; \
 	$(GOOSE) down-to $$VALUE
 
-db-reset:
-	$(GOOSE) reset
-
 db-status:
 	$(GOOSE) status 
+
+db-reset:
+	$(GOOSE) reset
 
 db-create:
 	@read -p "Migration name: " VALUE; \
@@ -41,11 +38,6 @@ db-create:
 
 go-tools:
 	go install golang.org/x/tools/gopls@latest
-	go install github.com/pressly/goose/v3/cmd/goose@latest
 	go install github.com/go-delve/delve/cmd/dlv@latest
+	go install github.com/pressly/goose/v3/cmd/goose@latest
 
-.PHONY: build compile run test
-.PHONY: db-up db-up-by-one db-up-to db-down db-down-to
-.PHONY: db-reset db-status db-create
-.PHONY: go-install
- 
